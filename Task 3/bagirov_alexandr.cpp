c809fd9602316bd66e77b5e199fbae7133077aa0
Alexandr Bagirov <a.d.bagirov@gmail.com>
2019-05-12 00:45:58 +0500

    Add tests

--- Task 3/bagirov_alexandr.cpp
+++ Task 3/bagirov_alexandr.cpp
@@ -6,51 +6,50 @@ template <class T>
 class SmartPtr
 {
 private:
-	T* ptr;
-	size_t* ref_cnt;
+    T* ptr;
+    size_t* ref_cnt{};
 
 public:
-	explicit SmartPtr(T* ptr)
-	{
-		this->ptr = ptr;
-		this->ref_cnt = new size_t();
-		this->*ref_cnt = 1;
-	}
+    explicit SmartPtr()
+    {
+        this->ptr = nullptr;
+        this->ref_cnt = nullptr;
+    }
 
     ~SmartPtr()
     {
         Release();
     }
 
-	void Release()
-	{
-		if (!this->ptr)
+    void Release()
+    {
+        if (!this->ptr)
         {
             return;
         }
-		
-        (*this->ref_cnt)--;
-        if (!*this->ref_cnt)
+
+        (*ref_cnt)--;
+        if (!this->ref_cnt)
         {
             delete ptr;
             delete ref_cnt;
         }
-        
-        this->ptr = 0;
-        this->ref_cnt = 0;
-	}
+
+        this->ptr = nullptr;
+        this->ref_cnt = nullptr;
+    }
 
     void Set(T* other)
     {
-        Release();
         this->ptr = other;
-        this->ref_cnt = 1;
+        this->ref_cnt = (size_t*)malloc(sizeof(size_t));
+        *ref_cnt = 1;
     }
 
-	T* Get()
-	{
-		return this->ptr;
-	}
+    T Get()
+    {
+        return *ptr;
+    }
 
     T* operator->()
     {
@@ -62,12 +61,43 @@ public:
         return *this->ptr;
     }
 
-	SmartPtr& operator=(SmartPtr& other)
-	{
-		Release();
-		this->ptr = other.ptr;
-		this->ref_cnt = other.ref_cnt;
-		(*ref_cnt)++;
-		return *this;
-	}
+    SmartPtr& operator=(SmartPtr& other)
+    {
+        if (*ref_cnt > 0)
+        {
+            Release();
+        }
+        if (this != &other)
+        {
+            this->ptr = other.ptr;
+            this->ref_cnt = other.ref_cnt;
+            (*ref_cnt)++;
+        }
+        return *this;
+    }
+
+    void Print()
+    {
+        cout << "Object: " << Get() << "\nNumber of links: " << *ref_cnt << "\n========\n";
+    }
 };
+
+
+int main() {
+    auto p1 = SmartPtr<string>();
+    p1.Set(new string("test 1"));
+    p1.Print();
+
+    auto p2 = SmartPtr<string>();
+    p2.Set(new string("test 2"));
+    p2.Print();
+
+    p1 = p2;
+    p1.Print();
+
+    p2.Release();
+    p1.Print();
+
+    p1.Release();
+    return 0;
+}
