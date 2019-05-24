bf227c9889baa029c095a6bdd46a19f49b6aa995
inexperienced-mf <34794882+inexperienced-mf@users.noreply.github.com>
2019-05-25 00:44:01 +0500

    Update islamov_amir.cpp
    Теперь должна компилиться.

--- Task 2/islamov_amir.cpp
+++ Task 2/islamov_amir.cpp
@@ -2,7 +2,6 @@
 #include <vector>
 #include <map>
 #include <set>
-
 using namespace std;
 
 
@@ -11,9 +10,9 @@ private:
     char* _base_ptr;
     size_t _size;
     vector<set<unsigned long>> _free_blocks_by_order;
-    map<unsigned long, uint> _order_of_allocated_block;
+    map<unsigned long, unsigned long> _order_of_allocated_block;
 
-    void _try_create_at_order(uint order) {
+    void _try_create_at_order(unsigned long order) {
         auto i = order;
         for(; _free_blocks_by_order[i].empty(); i++)
             if (i == _free_blocks_by_order.size()) return;
@@ -24,7 +23,7 @@ private:
         _free_blocks_by_order[order].insert(offset);
     }
 
-    unsigned long _get_buddy_offset(ptrdiff_t offset, uint order) {
+    unsigned long _get_buddy_offset(ptrdiff_t offset, unsigned long order) {
         auto size = static_cast<size_t>(1 << order);
         if (offset % size)
             return offset - size;
@@ -39,14 +38,14 @@ public:
         for (int i = 0; i < max_order; ++i)
             _free_blocks_by_order[i] = set<unsigned long>();
         _free_blocks_by_order[max_order].insert(0);
-        _order_of_allocated_block = map<unsigned long, uint>();
+        _order_of_allocated_block = map<unsigned long, unsigned long>();
     }
 
     void* Allocate(size_t size) {
         if (size > _size)
             return nullptr;
         auto __size = 1;
-        uint order = 0;
+        unsigned long order = 0;
         for (; __size < size; __size <<= 1)
             order++;
         if (_free_blocks_by_order[order].empty())
@@ -84,15 +83,44 @@ public:
             result.emplace_back(pair<unsigned long, long>(kvp.first, 1 << kvp.second));
         return result;
     }
+
+    ~BuddyAllocator() {
+        free(_base_ptr);
+    }
 };
 
+void print_dump(vector<pair<unsigned long, long>> dump) {
+    for (auto p: dump)
+        cout << p.second << "B block at " << p.first << " byte offset.\n";
+    cout << '\n';
+}
 
 int main() {
     auto buddy_allocator = BuddyAllocator();
+
     auto a = buddy_allocator.Allocate(1500);
-    auto dump_a = buddy_allocator.Dump(); //returns a vector of pairs kind of <offset, size>;
+    print_dump(buddy_allocator.Dump());
+
     auto b = buddy_allocator.Allocate(380);
+    print_dump(buddy_allocator.Dump());
+
+    auto c = buddy_allocator.Allocate(460);
+    print_dump(buddy_allocator.Dump());
+
+    buddy_allocator.Free(b);
+    print_dump(buddy_allocator.Dump());
+
+    b = buddy_allocator.Allocate(128);
+    print_dump(buddy_allocator.Dump());
+
+    auto d = buddy_allocator.Allocate(256);
+    print_dump(buddy_allocator.Dump());
+
     buddy_allocator.Free(a);
-    dump_a = buddy_allocator.Dump();
+    print_dump(buddy_allocator.Dump());
+
+    a = buddy_allocator.Allocate(1025);
+    print_dump(buddy_allocator.Dump());
+
     return 0;
 }
