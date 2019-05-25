77f02ddc1906a8f5ada8043fd194e073b1f96d32
Alexandr Bagirov <a.d.bagirov@gmail.com>
2019-05-25 16:21:56 +0500

    Add tests

--- Task 2/bagirov_alexandr.cpp
+++ Task 2/bagirov_alexandr.cpp
@@ -1,183 +1,177 @@
+#include <stdio.h>
 #include <iostream>
+#include <vector>
+#include <map>
 #include <cmath>
+
+/*
+Программа протестирована в C++ 14. Вывод доступен по ссылке:
+https://drive.google.com/file/d/1OygWP-lVG7jTMl3oTUFfpsHLYvM4mbWQ/view?usp=sharing
+*/
 
 using namespace std;
 
-
 struct Node
 {
-    int level;
-    bool empty;
-    Node *prev;
-    Node *next;
+public:
+    Node(void* start, size_t size)
+    {
+        this->start = start;
+        this->size = size;
+    }
+    void* start;
+    size_t size;
 };
-
 
 class NodeList
 {
-private:
-    int level;
-
 public:
-    Node *first;
-
-    explicit NodeList(int level)
-    {
-        this->first = nullptr;
-        this->level = level;
-    }
-
-    void Add(Node *node)
-    {
-        if (first == nullptr)
-        {
-            first = node;
-            node->prev = node;
-            node->next = node;
-            return;
-        }
-
-        Node *current = first;
-        while (true)
-        {
-            if (current->next != first && size_t(current) > size_t(node))
-            {
-                current = current->next;
-            }
-            else
-            {
-                break;
-            }
-        }
-
-        Node *prev = current->prev;
-        node->next = prev->next;
-        node->prev = prev;
-        current->prev = node;
-        current->next = node;
-    }
-
-    void Remove(Node *node)
-    {
-        if (first->next == first && first == node)
-        {
-            first = nullptr;
-            return;
-        }
-
-        Node *prev = node->prev;
-        Node *next = node->next;
-        prev->next = next;
-        next->prev = prev;
-    }
+    vector<Node> chunks;
 };
 
-
-class Buddy
-{
+class Buddy {
 private:
-    int min_level;
-    int max_level;
+    void *start;
     size_t size;
+    NodeList layers[32];
+    map<void*, int> sizes;
 
-    NodeList **layers;
-    void *pool;
-
-public:
-    explicit Buddy(int min_level = 4, int max_level = 16)
+    void mergeNodes(int power)
     {
-        if (min_level < 4)
+        auto item = layers[power].chunks.back();
+        int len = layers[power].chunks.size() - 1;
+        void* current_size = item.start;
+        auto end = (uintptr_t)current_size + item.size;
+
+        for (int i = 0; i < len; i++)
         {
-            throw invalid_argument("Min level cannot be less than 4");
+            void *currPtr = layers[power].chunks.at(i).start;
+            void *currEnd = (void *)((size_t)currPtr + layers[power].chunks[i].size);
+            if ((uintptr_t)currPtr - end == 0 || (uintptr_t)current_size - (uintptr_t)currEnd == 0)
+            {
+                sizes.erase(layers[power].chunks.back().start);
+                layers[power].chunks.pop_back();
+                sizes.erase(layers[power].chunks.back().start);
+                layers[power].chunks.pop_back();
+
+                layers[power + 1].chunks.emplace_back(((uintptr_t)currPtr == end) ? currPtr : current_size, item.size * 2);
+                mergeNodes(power + 1);
+                return;
+            }
         }
-        if (max_level > 16)
-        {
-            throw invalid_argument("Max level cannot be more than 16");
-        }
-        if (max_level < min_level)
-        {
-            throw invalid_argument("Max level cannot be less than min level");
-        }
-
-        this->min_level = min_level;
-        this->max_level = max_level;
-
-        this->size = (int)pow(2, max_level);
-        pool = malloc(size);
-
-        this->layers = new NodeList*[max_level - min_level + 1];
-
-        for (int i = 0; i <= max_level - min_level; i++)
-        {
-            layers[i] = new NodeList(i + min_level + 1);
-        }
-
-        Node *node = (Node*)this->pool;
-        node->empty = true;
-        node->level = max_level;
-
-        layers[max_level - min_level - 1]->Add(node);
     }
 
-    void *Alloc(size_t block_size)
+public:
+    explicit Buddy(size_t length)
     {
-        size_t needed_size = (int)pow(2, min_level);
-        int needed_level = min_level;
+        size = getPower(length);
+        start = malloc(length);
+        int index = (int)log2(length);
+        layers[index] = NodeList();
+        layers[index].chunks.emplace_back(start, length);
+    }
 
-        while (needed_size < block_size + sizeof(Node))
+    void* Alloc(size_t needed_size)
+    {
+        needed_size = getPower(needed_size);
+        auto needed_level = (int)log2(needed_size);
+
+        while (layers[needed_level].chunks.empty() && needed_level <= 32)
         {
-            needed_size *= 2;
             needed_level++;
         }
 
-        if (needed_level > max_level)
+        if (needed_level > 32)
         {
             throw invalid_argument("Block size too big");
         }
 
-        int current_level = needed_level;
-        auto current_block = layers[current_level - min_level]->first;
-
-        if (layers[needed_level - min_level]->first == nullptr) {
-            while (layers[current_level - min_level]->first == nullptr && current_level <= max_level) {
-                current_level++;
-            }
-
-            if (current_level > max_level) {
-                throw invalid_argument("Block size too big");
-            }
-
-            current_block = layers[current_level - min_level]->first;
-
-            while (current_level != needed_level) {
-                layers[current_level - min_level]->Remove(current_block);
-
-                Node* buddyNode = (Node*) ((size_t(current_block) + sizeof(current_block) / 2));
-                buddyNode->empty = true;
-                buddyNode->level = current_block->level - 1;
-
-                current_block->level--;
-
-                layers[current_block->level - min_level]->Add(current_block);
-                layers[buddyNode->level - min_level]->Add(buddyNode);
-
-                current_level--;
-            }
+        if (layers[needed_level].chunks.empty())
+        {
+            throw invalid_argument("Not enough memory");
         }
 
-        current_block->empty = false;
-        layers[current_block->level - min_level]->Remove(current_block);
-        return current_block;
+        auto item = layers[needed_level].chunks.back();
+        layers[needed_level].chunks.pop_back();
+        auto ptr = item.start;
+
+        while (pow(2, needed_level) != needed_size)
+        {
+            needed_level--;
+            ptr = ((char*)ptr + (int)pow(2, needed_level));
+            sizes[ptr] = (int)pow(2, needed_level);
+            layers[needed_level].chunks.emplace_back(ptr, (int)pow(2, needed_level));
+        }
+
+        ptr = ((char*)ptr + (int)pow(2, needed_level));
+        sizes[ptr] = (int)pow(2, needed_level);
+        return ptr;
     }
 
-    void Free(void *ptr)
+    void Free(void* ptr)
     {
-        auto block_address = (size_t)ptr;
-        Node* block = (Node*) block_address;
+        int s = sizes[ptr];
+        auto power = (int)log2(s);
 
-        auto block_size = (size_t)pow(2, block->level);
-        block->empty = true;
+        if (!layers[power].chunks.empty())
+        {
+            layers[power].chunks.emplace_back((void*)ptr, (int)s);
+            mergeNodes(power);
+            return;
+        }
 
-        layers[block->level - min_level]->Remove(block);
+        layers[power].chunks.emplace_back(ptr, s);
+        sizes.erase(ptr);
+        mergeNodes(power);
+    }
+
+    void Dump()
+    {
+        for (auto & FreeChunk : layers)
+        {
+            for (auto & chunk : FreeChunk.chunks)
+            {
+                cout << chunk.size << " " << chunk.start << endl;
+            }
+        }
+    }
+
+    static int getPower(int number)
+    {
+        auto power = (int)log2(number);
+        if (pow(2, power) < number)
+        {
+            return (int)pow(2, power + 1);
+        }
+        return (int)pow(2, power);
     }
 };
+
+int main()
+{
+    auto buddy = Buddy(1024);
+
+    cout << "Изначальное состояние" << endl;
+    buddy.Dump();
+
+    cout << "Выделяем память так, чтобы получить наименьший возможный блок" << endl;
+    void *one = buddy.Alloc(6);
+    buddy.Dump();
+
+    cout << "Освобождаем блок" << endl;
+    buddy.Free(one);
+    buddy.Dump();
+
+    cout << "Выделяем блок другого уровня" << endl;
+    void *two = buddy.Alloc(54);
+    buddy.Dump();
+
+    cout << "Освобождаем первый блок" << endl;
+    buddy.Free(two);
+    buddy.Dump();
+
+    cout << "Итоговое состояние (должно совпадать с изначальным)" << endl;
+    buddy.Dump();
+
+    return 0;
+}
